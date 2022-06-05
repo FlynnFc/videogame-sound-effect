d8ebc159f16fccb0111ac257602739aacb4fe54e
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-06-05 19:31:46 +0100

    Modal on guess check

--- src/components/Easy.jsx
+++ src/components/Easy.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { CorrectEasy } from "./ui/modals/CorrectEasy";
 
 export const Easy = () => {
+  const [guessHandler, setGuessHandler] = useState(false);
+
+  const answerSubmitHandler = () => {
+    setGuessHandler(true);
+  };
+
   return (
     <div
       className="flex flex-col text-center justify-center align-middle"
@@ -21,13 +27,14 @@ export const Easy = () => {
           className="input input-bordered w-full max-w-2xl text-5xl h-full rounded-r-none text-center shadow-x p-4"
         />
         <button
+          onClick={answerSubmitHandler}
           type="button"
           className="btn btn-info h-full rounded-l-none shadow-xl text-2xl"
         >
           Guess
         </button>
       </div>
-      <CorrectEasy></CorrectEasy>
+      {guessHandler && <CorrectEasy></CorrectEasy>}
     </div>
   );
 };
--- src/components/Hard.jsx
+++ src/components/Hard.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { CorrectEasy } from "./ui/modals/CorrectEasy";
 
 export const Hard = () => {
+  const [guessHandler, setGuessHandler] = useState(false);
+
+  const answerSubmitHandler = () => {
+    setGuessHandler((guessHandler) => !guessHandler);
+  };
+
   return (
     <div
       className="flex flex-col text-center justify-center align-middle"
@@ -20,12 +27,14 @@ export const Hard = () => {
           className="input input-bordered w-full max-w-2xl text-5xl h-full rounded-r-none text-center shadow-x p-4"
         />
         <button
+          onClick={answerSubmitHandler}
           type="button"
           className="btn btn-warning h-full rounded-l-none shadow-xl text-2xl"
         >
           Guess
         </button>
       </div>
+      {guessHandler && <CorrectEasy></CorrectEasy>}
     </div>
   );
 };
