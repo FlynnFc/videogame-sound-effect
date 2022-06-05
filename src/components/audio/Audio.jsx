e1f5f3e3a3d479def94507df1b79fbe41170e20c
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-06-05 23:05:29 +0100

    Audio update bug fixed

--- src/components/audio/Audio.jsx
+++ src/components/audio/Audio.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const Audio = (props) => {
+  const [source, setSource] = useState();
+  const audioRef = useRef();
+
+  useEffect(() => {
+    setSource(props.currentSource);
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.load();
+    }
+  });
   console.log("component rendered", props.currentSource);
   return (
     <div>
       {/* <button className="btn">play</button> */}
-      <audio controls>
-        <source type="audio/mpeg" src={props.currentSource} />
+      <audio ref={audioRef} controls>
+        <source type="audio/mpeg" src={source} />
         <source type="audio/ogg" src="" />
       </audio>
     </div>
